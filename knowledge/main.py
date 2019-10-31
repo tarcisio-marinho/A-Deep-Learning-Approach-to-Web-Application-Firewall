b9fd7cc34179b148c3875b7d621c7807e313c3d5
tarcisio <tarcisio_marinho09@hotmail.com>
2019-10-31 17:47:37 -0300

    basic python return

--- knowledge/main.py
+++ knowledge/main.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python3.6
 import sys
+import os
+import random
 
+def is_valid_file(path):
+    if(not os.path.isfile(path)):
+        return False
+    return True
+
+def identify(payload):
+    return random.choice(['xss', 'sqli', 'path-traversal', 'valido', 'rce', 'xxe'])
+        
 if __name__ == "__main__":
-    print(sys.argv[1])
-    print("TESTE")
+    if(len(sys.argv)< 2):
+        print('invalid sintaxe')
+        sys.exit()
+        
+    if(is_valid_file(sys.argv[1])):
+        with open(sys.argv[1], 'rb') as f:
+            payload = f.read()
+        print(identify(payload))
+        
+    else:
+        print("invalid input")
