eb32900b0b164a730156df26a8ada926c4710ed7
tarcisio <tarcisio_marinho09@hotmail.com>
2019-11-02 12:02:51 -0300

    knowledge ready and trained

--- knowledge/main.py
+++ knowledge/main.py
@@ -2,6 +2,7 @@
 import sys
 import os
 import random
+import subprocess
 
 def is_valid_file(path):
     if(not os.path.isfile(path)):
@@ -11,18 +12,29 @@ def is_valid_file(path):
 def delete_input_file(path):
     os.remove(path)
 
-def identify(payload):
-    return random.choice(['xss', 'sqli', 'path-traversal', 'valido', 'rce', 'xxe'])
-        
+def identify(model_path, payload):
+    command = "fasttext predict %s %s" %(model_path, payload)
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE,
+                               stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+    stdout = process.stdout.readlines()
+    stderr = process.stderr.readlines()
+    
+    if(stderr):
+        print(stderr)
+    else:
+        label = [payload.replace("__label__", "").replace("\n", "") for payload in stdout]
+        print(label)
+            
 if __name__ == "__main__":
+    model = 'models/model2.bin'
     if(len(sys.argv)< 2):
-        print('invalid sintaxe')
+        print('invalid sintaxe, input file missing?')
         sys.exit()
         
-    if(is_valid_file(sys.argv[1])):
-        with open(sys.argv[1], 'rb') as f:
-            payload = f.read()
-        print(identify(payload))
-        
+    payload = sys.argv[1]
+    if(is_valid_file(payload)):
+        identify(model, payload)
+
     else:
         print("invalid input file")
