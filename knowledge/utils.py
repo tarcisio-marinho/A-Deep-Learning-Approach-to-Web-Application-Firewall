13c0d52bdb81ecdac4f1870e6689084fdf12692e
tarcisio <tarcisio_marinho09@hotmail.com>
2020-01-30 16:10:35 -0300

    update help msg

--- knowledge/utils.py
+++ knowledge/utils.py
@@ -23,10 +23,14 @@ def error(msg, exception, status_code):
 def print_help(file):
     if(file == 'model'):
         print('usage: ')
-        print('python create_model.py dataset_path new_model_path fasttext_path')
+        print('python create_model.py dataset_path new_model_path fasttext_path\n')
+        print("[dataset_path] has to be a valid dataset to train")
+        print("[new_model_path] filename to the new model that will be trained")
+        print("[fasttext_path] is the path to the fasttext binary. this project contains on disk the binary")
         sys.exit(1)
         
     elif(file == 'dataset'):
         print('usage: ')
-        print('python create_dataset.py dataset_name')
+        print('python create_dataset.py dataset_name\n')
+        print('[dataset_name] is the name of the new dataset that will be created')
         sys.exit(1)
